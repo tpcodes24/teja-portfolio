c316aa30803a0000f109f63aecc0ca6e4984bf31
tpcodes24 <p.tejasree089@gmail.com>
2024-06-19 19:58:23 -0400

    Portfolio-Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,8 +27,8 @@ module.exports = {
       colors: {
         primary: "#1c1c22",
         accent: {
-          DEFAULT: "#00ff99",
-          hover: "#00e187",
+          DEFAULT: "#00ff99", /*00ff99*/
+          hover: "#00e187",/*"#00e187",*/
         }
 
       },
