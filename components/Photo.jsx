4e24bcf269e0d70bdae8a7c028f29d237fddad6d
tpcodes24 <p.tejasree089@gmail.com>
2024-06-19 22:16:08 -0400

    deployment-6

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -3,6 +3,10 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 
+const photos = {
+  image: '/assets/HEADSHOT.png'
+}
+
 const Photo = () => {
   return (
     <div className="w-full h-full relative">
@@ -21,7 +25,7 @@ const Photo = () => {
             }} 
             
             className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
-                <Image src="/assets/HEADSHOT.PNG" priority quality={100} fill alt="" className="object-contain"/>
+                <Image src={photos.image} priority quality={100} fill alt="" className="object-contain"/>
             </motion.div>
 
             {/*Circle*/}
